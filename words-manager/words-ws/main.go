560c55e8f2209d17a239952ed690c1bd82f5ece2
david.alonso <david.alonsobadia@gmail.com>
2022-02-07 18:13:16 +0100

    Add base Go web service

---
+++ words-manager/words-ws/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+//   "gorm.io/driver/postgres"
+//   "gorm.io/gorm"
+  "fmt"
+)
+
+
+func main() {
+
+    fmt.Println("Here it is a Go program")
+
+//     dsn := "host=db user=postgres password=postgres dbname=postgres port=5432"
+//     db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+}
+
+
